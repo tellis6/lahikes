c638b3f172a4816ecc6d40418c4fa210ef0fa951
tellis6 <tellis6@calstatela.edu>
2020-05-03 14:04:29 -0700

    Modified Forums

--- src/lahikes/servlet/Forums.java
+++ src/lahikes/servlet/Forums.java
@@ -1,6 +1,11 @@
 package lahikes.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +15,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import lahikes.model.Forum;
 
@@ -23,17 +29,56 @@ public class Forums extends HttpServlet {
 
     public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		
-		List<Forum> forums = new ArrayList<Forum>();
-		forums.add( new Forum(1, "General Discussion", 0));
-		forums.add( new Forum(2, "Trail Information", 0));
-		
-		getServletContext().setAttribute("forums", forums);
-		
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/Forums.jsp").forward(request, response);
+		
+		HttpSession session = request.getSession();
+        String utype = (String) session.getAttribute( "utype" );
+        String log = "Login";
+        
+        if( utype != null )
+        	log = "Logout";
+				
+        List<Forum> forums = new ArrayList<Forum>();
+        Connection c = null;
+        try
+        {
+            String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu82";
+            String username = "cs3220stu82";
+            String password = "eSkffhp3";
+
+            c = DriverManager.getConnection( url, username, password );
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( 
+            		"select f.id, f.name, count(t.id) "
+            		+ "from lah_topics t "
+            		+ "right join lah_forums f on t.forum_id = f.id "
+            		+ "group by f.name;" );
+
+            while( rs.next() )
+                forums.add( new Forum( rs.getInt( "f.id" ), rs.getString( "f.name" ), rs.getInt( "count(t.id)" ) ));
+        }
+        catch( SQLException e )
+        {
+            throw new ServletException( e );
+        }
+        finally
+        {
+            try
+            {
+                if( c != null ) c.close();
+            }
+            catch( SQLException e )
+            {
+                throw new ServletException( e );
+            }
+        }
+
+        request.setAttribute( "log", log );
+        request.setAttribute( "utype", utype );
+        request.setAttribute( "forums", forums );
+        request.getRequestDispatcher( "/WEB-INF/Forums.jsp" ).forward( request, response );
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

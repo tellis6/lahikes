c4d99581c75673d42a19ea42781a6b263db9ce03
tellis6 <tellis6@calstatela.edu>
2020-05-03 14:06:09 -0700

    Modified Topics

--- src/lahikes/servlet/Topics.java
+++ src/lahikes/servlet/Topics.java
@@ -1,18 +1,22 @@
 package lahikes.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import lahikes.model.Topic;
-import lahikes.model.Forum;
 
 @WebServlet("/Topics")
 public class Topics extends HttpServlet {
@@ -22,30 +26,75 @@ public class Topics extends HttpServlet {
         super();
     }
     
-    public void init(ServletConfig config) throws ServletException {
-		super.init(config);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		List<Topic> topics = new ArrayList<Topic>();
-		
-		getServletContext().setAttribute("topics", topics);	
-	}
-    
-    private Forum getForum(Integer id)
-    {
-        @SuppressWarnings("unchecked")
-		List<Forum> forums = (List<Forum>) getServletContext().getAttribute("forums");
-
-        for(Forum forum : forums)
-            if(forum.getId().equals(id)) return forum;
-
-        return null;
-    }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+    	HttpSession session = request.getSession();
+        String utype = (String) session.getAttribute( "utype" );
+        String log = "Login";
+        if( utype != null )
+        	log = "Logout";
+        
 		Integer id = Integer.valueOf(request.getParameter("id"));
+		List<Topic> topics = new ArrayList<Topic>();
+		String fName = "";
 		
-		request.setAttribute("forum", getForum(id));
+        Connection c = null;
+        try
+        {
+            String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu82";
+            String username = "cs3220stu82";
+            String password = "eSkffhp3";           
+
+            c = DriverManager.getConnection( url, username, password );
+            String sql = 
+            		"select t.id, t.subject, u.first_name, t.message, t.num_of_replies, date_format(t.last_post_time, '%m/%d/%Y %l:%i%p'), t.forum_id\n" + 
+            		"    from lah_topics t \n" + 
+            		"    inner join lah_users u on t.author_id = u.id\n" + 
+            		"    where t.forum_id = ?;";           
+            PreparedStatement pstmt = c.prepareStatement( sql );
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+
+            while( rs.next() ) 
+                topics.add( new Topic( 
+                		rs.getInt( "t.id" ), 
+                		rs.getString( "t.subject" ), 
+                		rs.getString( "u.first_name" ), 
+                		rs.getString( "t.message" ), 
+                		rs.getInt( "t.num_of_replies" ), 
+                		rs.getString( "date_format(t.last_post_time, '%m/%d/%Y %l:%i%p')" ), 
+                		rs.getInt( "t.forum_id" )));   
+            
+            String sql2 = "select name from lah_forums\n" + 
+            		"where id = ?;";
+            PreparedStatement pstmt2 = c.prepareStatement( sql2 );
+            pstmt2.setInt(1, id);
+            ResultSet rs2 = pstmt2.executeQuery();
+
+            while( rs2.next() ) 
+            	fName = rs2.getString( "name" );
+            
+        }
+        catch( SQLException e )
+        {
+            throw new ServletException( e );
+        }
+        finally
+        {
+            try
+            {
+                if( c != null ) c.close();
+            }
+            catch( SQLException e )
+            {
+                throw new ServletException( e );
+            }
+        }
+               
+        request.setAttribute( "log", log );
+        request.setAttribute( "utype", utype );
+        request.setAttribute( "fName", fName );       
+        request.setAttribute( "topics", topics );
 		request.getRequestDispatcher("/WEB-INF/Topics.jsp").forward(request, response);
 
 	}

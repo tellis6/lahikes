f41a6173fdadfd0261e3c3ddbc670d5922932f11
tellis6 <tellis6@calstatela.edu>
2020-05-03 14:04:49 -0700

    Modified Home

--- src/lahikes/servlet/Home.java
+++ src/lahikes/servlet/Home.java
@@ -1,31 +1,48 @@
 package lahikes.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-@WebServlet("/Home")
+@WebServlet(urlPatterns = "/Home", loadOnStartup = 2)
 public class Home extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     public Home() {
         super();
     }
+    
+    public void init(ServletConfig config) throws ServletException {
+		super.init( config );
+
+        try
+        {
+            Class.forName( "com.mysql.jdbc.Driver" );
+        }
+        catch( ClassNotFoundException e )
+        {
+            throw new ServletException( e );
+        }
+		
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		if(request.getParameter("user")!=null)
-		{
-			String username = request.getParameter("user");		
-			request.setAttribute("user", username);
-		}
+		HttpSession session = request.getSession();
+        String utype = (String) session.getAttribute( "utype" );
+        String log = "Login";
+        
+        if( utype != null )
+        	log = "Logout";
+        
+        request.setAttribute( "log", log );
 		request.getRequestDispatcher("/WEB-INF/Home.jsp").forward(request, response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	}
 
 }
